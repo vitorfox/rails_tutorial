e7d5b31ac9581e939856ffe8bfd779a209e3c337
Vitor Teixeira <vitorfox@gmail.com>
2015-01-20 18:30:13 -0200

    adding-some-validation
    http://guides.rubyonrails.org/getting_started.html#adding-some-validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150116164247) do
+ActiveRecord::Schema.define(version: 20150120194526) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "nome"
